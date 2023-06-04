0836019749a7ab9e54a8ab3db820463714be094c
Arman <Arman.sajadi10@gmail.com>
2023-06-04 22:37:52 +0330

    Im the winner

---
+++ app.js
@@ -0,0 +1,4 @@
+
+var style = document.createElement('style');
+style.innerHTML = "@font-face { font-family: 'iranyekan'; src: url('fonts/Mikhak-DS1-FD-Bold.ttf') format('truetype'); font-weight:bold;}";
+document.head.appendChild(style);
